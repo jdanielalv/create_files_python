cc8df03f4aa95631554bd1d6d28d71b6b496b064
J Daniel Alvarado <informatico.alvarado@gmail.com>
2022-12-17 20:53:19 -0300

    add ciclo for para hacer 4 tests

--- tarea05.py
+++ tarea05.py
@@ -7,18 +7,20 @@ for x in range (5,9):
     inicio = datetime.strptime(f"{datetime.now()}", date_format_str)
 
     file = open("tarea05.txt","w")
-    file.write("inicio: " + f"{inicio}" )
+    file.write(" Inicio test " + f"{x}" + " ceros. " + "{:,}".format(rango) + " numeros.")
+    file.write(" Fecha hora de inicio: " + f"{inicio}" )
 
+    #ciclo de n cantidad de números
     for i in range (0, rango):
-        file.write("nro. " + f"{i}")
+        file.write("numero. " + f"{i}")
 
     fin = datetime.strptime(f"{datetime.now()}", date_format_str) 
     diff = fin - inicio
-    file.write(" final: " + f"{fin}")
-    file.write("diferencia: " + f"{diff}")
+    file.write(" Fecha hora fin: " + f"{fin}")
+    file.write(" Total tiempo de ejecución: " + f"{diff}")
     file.close()
 
-    print("******* test " + f"{x}" + " ceros *******  " + f"{rango}" + " numeros ******* ")
-    print("inicio: " + f"{inicio}" + " >> final : " + f"{fin}")
-    print("diferencia: " + f"{diff}")
+    print(" ***** Inicio test " + f"{x}" + " ceros *******  " + "{:,}".format(rango) + " numeros ******* ")
+    print(" Fecha hora de inicio: " + f"{inicio}" + " >> Fecha hora fin : " + f"{fin}")
+    print(" Total tiempo de ejecución con : " + "{:,}".format(rango) + " numeros -> " + f"{diff}")
     rango = rango * 10
