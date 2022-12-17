0e981c7a8598842cfc07bec1d4d40c8ec08e3076
J Daniel Alvarado <informatico.alvarado@gmail.com>
2022-12-17 02:09:25 -0300

    add ciclo for para hacer 4 tests

--- tarea05.py
+++ tarea05.py
@@ -1,21 +1,24 @@
 import os
 from datetime import datetime
 
-date_format_str = '%Y-%m-%d %H:%M:%S.%f'
-inicio = datetime.strptime(f"{datetime.now()}", date_format_str)
+rango = 100000
+for x in range (5,9):
+    date_format_str = '%Y-%m-%d %H:%M:%S.%f'
+    inicio = datetime.strptime(f"{datetime.now()}", date_format_str)
 
-file = open("tarea05.txt","w")
-file.write("inicio: " + f"{inicio}")
+    file = open("tarea05.txt","w")
+    file.write("inicio: " + f"{inicio}" )
 
-for i in range (1, 100001):
-    file.write("nro. " + f"{i}")
+    for i in range (0, rango):
+        file.write("nro. " + f"{i}")
 
-fin = datetime.strptime(f"{datetime.now()}", date_format_str) 
-diff = fin - inicio
-file.write(" final: " + f"{fin}")
-file.write("diferencia: " + f"{diff}")
-file.close()
+    fin = datetime.strptime(f"{datetime.now()}", date_format_str) 
+    diff = fin - inicio
+    file.write(" final: " + f"{fin}")
+    file.write("diferencia: " + f"{diff}")
+    file.close()
 
-print("inicio: " + f"{inicio}")
-print("final : " + f"{fin}")
-print("diferencia: " + f"{diff}")
+    print("******* test " + f"{x}" + " ceros *******  " + f"{rango}" + " numeros ******* ")
+    print("inicio: " + f"{inicio}" + " >> final : " + f"{fin}")
+    print("diferencia: " + f"{diff}")
+    rango = rango * 10
